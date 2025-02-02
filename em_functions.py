03fa3c28d7c14e089eaffd7e36be33703ac001de
nenad <ndvasily@outlook.com>
2025-02-02 20:08:12 +0100

    Feeding part is complete: - data save with CSV - list every meal - total ml per day - code clean up

--- em_functions.py
+++ em_functions.py
@@ -12,13 +12,58 @@ def write_ml_log(ml_arg, filepath='ml_log.txt'):
         writer.writerow(ml_arg)
 
 
-def get_sleep_log(todos_arg, filepath='Todos.txt' ):
+def get_sleep_log(filepath='Todos.txt' ):
     with open(filepath , 'r') as file:
         ml_logs = file.readlines()    # mnogu poprofi e vaka
     return ml_logs
 
 
 def write_sleep_log(todos_arg, filepath='Todos.txt' ):
-    """ Saves the new Todo list. """  #Doc String
     with open(filepath, 'w') as file:  # zapishuvanje na nov file so skraten metod (WITH)
         file.writelines(todos_arg)
+
+
+# ----------  FEED  -----------
+# prikaz na sekoe hranenje
+def get_ml_log_str(ml_log):
+    ml_log_str = ''
+    for meal in ml_log:
+        # meal = list(meal)
+        # st.write(meal)
+        meal_edit = f"{meal[0]} - {meal[1]} ml - {meal[2]} - {meal[3]}\n"
+        ml_log_str = ml_log_str + meal_edit
+    return ml_log_str
+
+
+# konvertiranje na ml_log od DIC vo STR za print na dneven total
+def get_ml_str(total_dic):
+    result_ml = ''
+    for key_dose in total_dic:
+        result_ml = result_ml + (f"{key_dose}   =   "
+                                 f"{total_dic[key_dose][0] + total_dic[key_dose][1]} ml"
+                                 f"   =   M: {total_dic[key_dose][0]} ml"
+                                 f"   +   F: {total_dic[key_dose][1]} ml\n")
+    return result_ml
+
+
+# obrabotka na ml_log vo lista na total ispieno vo den:
+def get_total_ml(ml_log):
+    ml_log_dic = {}
+    for feed in ml_log:
+        key = str(feed[3])
+        if key not in ml_log_dic:
+            ml_log_dic[key] = [0,0]
+        milk_type = feed[2]
+        add_ml = int(feed[1])
+        match milk_type:
+            case "Mama's":
+                ml_log_dic[key][0] += add_ml
+            case "Formula":
+                ml_log_dic[key][1] += add_ml
+    sorted_dict_asc = {k: ml_log_dic[k] for k in sorted(ml_log_dic, reverse=True)}
+    return sorted_dict_asc
+
+
+# ---------- SLEEP  ----------
+def get_sleep_log_str(sleep_log):
+    d
--- emilija_webapp.py
+++ emilija_webapp.py
@@ -1,63 +1,79 @@
 import streamlit as st
 import em_functions
 
+# momentalna vozrast na Emilija
+# notiikacii za hranenje i spienje
 
+###  APLIKACIJA   ###
+
+# inputi od GUI:
 def ispieno():
-    ispi = st.session_state["ml_input"] #+ "\n"
-    vreme = str(st.session_state["ml_time"])
-    date = st.session_state["date_input"]
-    date = str(date)
-    milk_type=''
-    if st.session_state["ml_majka"]:
-        milk_type="Mamas"
-    elif st.session_state["ml_formula"]:
-        milk_type="Formula"
-    input_log = [] #f"[{vreme}, {ispi}, {milk_type}, {date}]\n"
-    input_log.extend([vreme, ispi, milk_type, date])
-    print(input_log)
-    #ml_log.append(input_log)
-    #print(ml_log)
-    #ml_log.sort(reverse=True)
-    em_functions.write_ml_log(input_log, filepath='ml_log.txt')
+    try:
+        ispi = int(st.session_state["ml_input"])
+        vreme = str(st.session_state["ml_time"])
+        date = st.session_state["date_input"]
+        date = str(date)
+        milk_type=''
+        if st.session_state["ml_majka"]:
+            milk_type="Mama's"
+        elif st.session_state["ml_formula"]:
+            milk_type="Formula"
+        input_log = [] #f"[{vreme}, {ispi}, {milk_type}, {date}]\n"
+        input_log.extend([vreme, ispi, milk_type, date])
+        em_functions.write_ml_log(input_log, filepath='ml_log.txt')
+    except UnicodeEncodeError:
+        st.toast("Само нумеричка вредност може да се внесе!")
+    except ValueError:
+        st.toast("Само нумеричка вредност може да се внесе!")
 
 
 
-# vnesi gi site hranenja
-ml_log = em_functions.get_ml_log()
-print("prvo chitanje na ml_log:", ml_log)
-
-# log vo denot koga e posledno pieno
-#key_date = "datumot"
-# log za sekoj den kolku e vkupno ispieno
-#ml_daily = {}
-# proverka dali spie ili e budna
-#sleep_check = {"if sleep then must wake, if wake must sleep"}
-
-# log za posledno spienje vo denot so total vreme
-#sleep_log = {"sleep-date":"22:30", "wake-date":"04:10"}
-# log za spieni saati dnevno
 
 # Web App GUI
 st.title("Лејди Емилија")  # naslovot go printa prv, podnaslov vtor i tn.
-st.subheader("Апликација за следење на јадење и спиење.")
 
-st.date_input("Datum", key="date_input", format="DD.MM.YYYY")
-st.time_input("Последно е јадена во:", key="ml_time", step=300)
+# ---------- FEED ----------
+# vnesi gi site obroci od ml_log.txt so CSV
+ml_log = em_functions.get_ml_log()
+ml_log.sort(reverse=True)
+
+st.subheader("Дневник за исхрана.")
+#datum i vreme za ml
+st.date_input("Датум:", key="date_input", format="DD.MM.YYYY")
+st.time_input("Време:", key="ml_time", step=300)
 st.text_input(label="Колку млеко има испиено?", placeholder="внеси ml",
               key="ml_input")
 st.button("Мајчино", key="ml_majka", on_click=ispieno)
 st.button("Формула", key="ml_formula", on_click=ispieno)
-ml_log_str = ""
-for meal in ml_log:
-    #meal = list(meal)
-    #st.write(meal)
-    meal_edit = f"{meal[0]} - {meal[1]} ml - {meal[2]} - {meal[3]}"
-    ml_log_str = ml_log_str + meal_edit
-st.text_area( "Лог за храна:", value=ml_log_str, height=68)
-st.time_input("Заспа во:", key="sleep_time", step=300)
-st.button("Ok", key="sleep_ok")
-st.time_input("Се разбуди во:", key="wakeup_time", step=300)
-st.button("Ok", key="wakeup_ok")
-#st.code(em_functions.get_sleep_log())
+# log vo denot koga e posledno pieno
+ml_log_str = em_functions.get_ml_log_str(ml_log)
+st.text_area( "Лог за храна:", value=ml_log_str, height=136)
+
+# ----- SLEEP ----------
+# vnesi gi site obroci od ml_log.txt so CSV
+sleep_log = em_functions.get_sleep_log()
+sleep_log.sort(reverse=True)
+
+#sleep_check = {"if sleep then must wake, if wake must sleep"}
+# log za posledno spienje vo denot so total vreme
+#sleep_log = {"sleep-date":"22:30", "wake-date":"04:10"}
+# log za spieni saati dnevno
+st.subheader("Дневник за спиење.")
+st.date_input("Датум:", key="date_sleep", format="DD.MM.YYYY")
+st.time_input("Време", key="sleep_time", step=300)
+st.button("Заспа", key="sleep_ok")
+st.button("Разбуди", key="wakeup_ok")
+sleep_log_str = em_functions.get_sleep_log_str(sleep_log)
+st.text_area( "Лог за сон:", value=sleep_log_str, height=136)
+
+# --------- Total na den --------
+st.subheader("Вкупно храна и сон во ден.")
+#spremanje i podreduvanje dnevni ml
+ml_log_dic = em_functions.get_total_ml(ml_log)
+
+# dnevnite ml od desc vo string za printanje:
+total_daily = em_functions.get_ml_str(ml_log_dic)
+st.text_area( "Вкупно испиени ml на ден:", value=total_daily, height=136)
+
 st.session_state  # dopolnitelen prozor koj ni dava realtime input
 # sekoj item mora da ima key za da bide prikazan vo state
